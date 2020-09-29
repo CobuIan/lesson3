2800fa3678b55e3a421e6d94eb904b42abb3a68f
CobuIan <72077601+CobuIan@users.noreply.github.com>
2020-09-29 18:31:21 -0400

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,16 @@
+print(2*2)
+print(2*3)
+print(2**2)
+print(2**3)
+print(2+2.0)
+print("reeee"*10)
+print(int("2")+int("2"))
+print(2**64)
+total_seconds=2**64
+total_min=total_seconds//60
+total_h=total_min//60
+total_d=total_h//24
+total_y=total_d//365
+total_gen=total_y//100
+anser=input("what is your name?")
+print("hello",anser)
